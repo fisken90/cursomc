8366d89eb0ce85b8e4a8c3b2a89f2fcad5a330f9
Tiago Silva <tprsilva@gmail.com>
2018-10-19 20:36:26 +0100

    Estado e cidade

--- src/main/java/com/tiagosilva/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/tiagosilva/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -13,12 +13,8 @@ import com.tiagosilva.cursomc.services.exceptions.ObjNotFoundException;
 public class ResourceExceptionHandler {
 
 	@ExceptionHandler(ObjNotFoundException.class)
-	public ResponseEntity<StandardError> objectNotFound(ObjNotFoundException e, HttpServletRequest request){
-		StandardError err = new StandardError(
-				HttpStatus.NOT_FOUND.value(), 
-				e.getMessage(), 
-				System.currentTimeMillis()
-			);
+	public ResponseEntity<StandardError> objectNotFound(ObjNotFoundException e, HttpServletRequest request) {
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
 }
