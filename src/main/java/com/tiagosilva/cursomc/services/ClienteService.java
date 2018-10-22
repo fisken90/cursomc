921b72b1aa34b53d9053282df1e2d37773b59b57
Tiago Silva <tprsilva@gmail.com>
2018-10-22 12:01:53 +0100

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/tiagosilva/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.tiagosilva.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tiagosilva.cursomc.domain.Cliente;
+import com.tiagosilva.cursomc.repositories.ClienteRepository;
+import com.tiagosilva.cursomc.services.exceptions.ObjNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente get(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjNotFoundException(
+				"Objeto não encontrado, ID: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
