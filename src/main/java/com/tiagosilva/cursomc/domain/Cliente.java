1b5f2cdd6c52678e7e696a279a6b23e832ba7c71
Tiago Silva <tprsilva@gmail.com>
2018-10-22 13:21:58 +0100

    Pedido, Pagamento, EstadoPagamento

--- src/main/java/com/tiagosilva/cursomc/domain/Cliente.java
+++ src/main/java/com/tiagosilva/cursomc/domain/Cliente.java
@@ -32,6 +32,8 @@ public class Cliente implements Serializable {
 	@ElementCollection
 	@CollectionTable(name="Telefones")
 	private Set<String> telefones = new HashSet<>();
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente() {
 		
@@ -102,6 +104,14 @@ public class Cliente implements Serializable {
 		this.telefones = telefones;
 	}
 
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -126,7 +136,5 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
 	
 }
