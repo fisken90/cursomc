bd3113eb39ab82bb97a7919c39006864e401d769
Tiago Silva <tprsilva@gmail.com>
2018-10-24 00:12:33 +0100

    Personalização do return da validação

--- src/main/java/com/tiagosilva/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/tiagosilva/cursomc/dto/CategoriaDTO.java
@@ -10,34 +10,35 @@ import com.tiagosilva.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer id;
-	@NotEmpty(message="Preenchimento obrigatório")
-	@Length(min=5, max=80, message="Tamanho entre 5-80 caracteres")
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min = 5, max = 80, message = "Tamanho entre 5-80 caracteres")
 	private String nome;
-	
-	public CategoriaDTO () {
-		
+
+	public CategoriaDTO() {
+
 	}
-	
-	public CategoriaDTO (Categoria obj) {
+
+	public CategoriaDTO(Categoria obj) {
 		this.id = obj.getId();
 		this.nome = obj.getNome();
 	}
-	
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
-	
 
 }
