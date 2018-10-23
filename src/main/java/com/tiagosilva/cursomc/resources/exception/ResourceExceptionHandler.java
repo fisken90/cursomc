ca9ec492689d133d028d2e98fcf2c89b3ccaa29a
Tiago Silva <tprsilva@gmail.com>
2018-10-23 17:10:47 +0100

    Apagar uma categoria com DELETE

--- src/main/java/com/tiagosilva/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/tiagosilva/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.tiagosilva.cursomc.services.exceptions.DataIntegrityException;
 import com.tiagosilva.cursomc.services.exceptions.ObjNotFoundException;
 
 @ControllerAdvice
@@ -16,5 +17,11 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> objectNotFound(ObjNotFoundException e, HttpServletRequest request) {
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 }
