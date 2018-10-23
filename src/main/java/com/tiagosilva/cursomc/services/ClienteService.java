63963e82a162438c3f381955712534f59225debe
Tiago Silva <tprsilva@gmail.com>
2018-10-23 16:11:23 +0100

    Atualizacao de Categoria com o metodo PUT, pequenos ajustes

--- src/main/java/com/tiagosilva/cursomc/services/ClienteService.java
+++ src/main/java/com/tiagosilva/cursomc/services/ClienteService.java
@@ -15,7 +15,7 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 	
-	public Cliente get(Integer id) {
+	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjNotFoundException(
 				"Objeto não encontrado, ID: " + id + ", Tipo: " + Cliente.class.getName()));
--- src/main/java/com/tiagosilva/cursomc/services/PedidoService.java
+++ src/main/java/com/tiagosilva/cursomc/services/PedidoService.java
@@ -15,7 +15,7 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 	
-	public Pedido get(Integer id) {
+	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjNotFoundException(
 				"Objeto não encontrado, ID: " + id + ", Tipo: " + Pedido.class.getName()));
